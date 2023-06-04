d603cba35ca8f43fe1d81776a3335c8ddee4f853
liangming666-good <541781560@qq.com>
2023-06-04 22:35:51 +0800

    first commit

---
+++ app/src/main/java/com/example/myapplication/App.java
@@ -0,0 +1,6 @@
+package com.example.myapplication;
+
+public class App {
+    public String title; // 标题
+    public String content; //内容
+}
---
+++ app/src/main/java/com/example/myapplication/News.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+public class News  {
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String title; // 标题
+    public String content; //内容
+}
+
